54403f0654f02bfedd56e9a7a339412ec4da30b9
mikechibuzor <iwuagwuchibuzor@gmail.com>
2021-11-20 09:02:52 +0100

    finished the basic styling and structure of the first-half section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,33 @@ module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      "space-mono": ["Space Mono", "monospace"],
+    },
+    extend: {
+      colors: {
+        "strong-cyan": "hsl(172, 67%, 45%)",
+        "very-dark-cyan": "hsl(183, 100%, 15%)",
+        "dark-grayish-cyan1": "hsl(186, 14%, 43%)",
+        "dark-grayish-cyan2": "hsl(184, 14%, 56%)",
+        "light-grayish-cyan1": "hsl(185, 41%, 84%)",
+        "light-grayish-cyan2": "hsl(189, 41%, 97%)",
+      },
+      height: {
+        height10: "10vh",
+        height15: "15vh",
+        height20: "20vh",
+        height30: "30vh",
+        height40: "40vh",
+        height50: "50vh",
+        height55: "55vh",
+        height60: "60vh",
+        height70: "70vh",
+        height80: "80vh",
+        height90: "90vh",
+        height100: "100vh",
+      },
+    },
   },
   variants: {
     extend: {},
